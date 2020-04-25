52a4e4af9cf88208427cc83bc7bac6fc9ec9d232
Hacene <hacene.upmc@gmail.com>
2020-04-25 15:48:55 +0200

    refactoring unit tests

--- src/test/java/com/mlaku/dev/kata/halloweencandies/HalloweenCandiesTest.java
+++ src/test/java/com/mlaku/dev/kata/halloweencandies/HalloweenCandiesTest.java
@@ -8,62 +8,112 @@ public class HalloweenCandiesTest {
 
     @Test
     public void returnsThankYouWhenHaving2packetsWith2candiesFor2children() {
-        String result = Halloween.trickOrTreat(2, new String[][]{{"candy", "apple", "candy"}, {"candy", "candy"}});
-        assertThat(result).isEqualTo("Thank you, strange uncle!");
+        int nbrChildren = 2;
+        String[][] candies = {{"candy", "apple", "candy"}, {"candy", "candy"}};
+        String expected = "Thank you, strange uncle!";
+
+        String result = Halloween.trickOrTreat(nbrChildren, candies);
+
+        assertThat(result).isEqualTo(expected);
     }
 
     @Test
     public void returnsTrickOrTreatWhenHavingNoChildren() {
-        String result = Halloween.trickOrTreat(0, new String[][]{{"candy", "apple", "candy"}, {"candy", "candy"}});
-        assertThat(result).isEqualTo("Trick or treat!");
+        int nbrChildren = 0;
+        String[][] candies = {{"candy", "apple", "candy"}, {"candy", "candy"}};
+        String expected = "Trick or treat!";
+
+        String result = Halloween.trickOrTreat(nbrChildren, candies);
+
+        assertThat(result).isEqualTo(expected);
     }
 
     @Test
     public void returnsTrickOrTreatWhenHavingNoPackets() {
-        String result = Halloween.trickOrTreat(2, new String[][]{});
-        assertThat(result).isEqualTo("Trick or treat!");
+        int nbrChildren = 2;
+        String[][] candies = {};
+        String expected = "Trick or treat!";
+
+        String result = Halloween.trickOrTreat(nbrChildren, candies);
+
+        assertThat(result).isEqualTo(expected);
     }
 
     @Test
     public void shouldHaveTheSameNbrPacketsAndNbrChildren() {
         String[][] candies = {{"candy", "apple"}, {"apple", "candy"}, {"candy", "apple"}};
-        assertThat(Halloween.trickOrTreat(2, candies)).isEqualTo("Trick or treat!");
+        int nbrChildren = 2;
+        String expected = "Trick or treat!";
+
+        String actual = Halloween.trickOrTreat(nbrChildren, candies);
+
+        assertThat(actual).isEqualTo(expected);
     }
 
     @Test
     public void shouldFailWhenOnlyOneChild() {
         String[][] candies = {{"candy", "apple"}};
-        assertThat(Halloween.trickOrTreat(1, candies)).isEqualTo("Trick or treat!");
+        int nbrChildren = 1;
+        String expected = "Trick or treat!";
+
+        String actual = Halloween.trickOrTreat(nbrChildren, candies);
+
+        assertThat(actual).isEqualTo(expected);
     }
 
     @Test
     public void returnsThankYouWhenEachChildHaveAtLeast2Candies() {
         String[][] candies = {{"candy", "candy"}, {"candy", "candy"}, {"candy", "candy"}};
-        assertThat(Halloween.trickOrTreat(3, candies)).isEqualTo(Halloween.THANK_YOU_STRANGE_UNCLE);
+        int nbrChildren = 3;
+        String expected = "Thank you, strange uncle!";
+
+        String actual = Halloween.trickOrTreat(nbrChildren, candies);
+
+        assertThat(actual).isEqualTo(expected);
     }
 
     @Test
     public void returnsTrickOrTreatWhenChildHaveOnlyOneCandy() {
         String[][] candies = {{"candy", "candy"}, {"candy", "candy"}, {"candy", "apple"}};
-        assertThat(Halloween.trickOrTreat(3, candies)).isEqualTo("Trick or treat!");
+        int nbrChildren = 3;
+        String expected = "Trick or treat!";
+
+        String actual = Halloween.trickOrTreat(nbrChildren, candies);
+
+        assertThat(actual).isEqualTo(expected);
     }
 
     @Test
     public void returnsTrickOrTreatWhenChildrenHaveDifferentAmountOfCandies() {
         String[][] candies = {{"candy", "candy"}, {"candy", "candy"}, {"candy", "candy", "candy", "apple"}};
-        assertThat(Halloween.trickOrTreat(3, candies)).isEqualTo("Trick or treat!");
+        int nbrChildren = 3;
+        String actual = Halloween.trickOrTreat(nbrChildren, candies);
+
+        String expected = "Trick or treat!";
+
+        assertThat(actual).isEqualTo(expected);
     }
 
     @Test
     public void returnsTrickOrTreatWhenSomeChildGotABomb() {
         String[][] candies = {{"candy", "apple", "candy"}, {"candy", "candy"}, {"candy", "bomb", "candy"}};
-        assertThat(Halloween.trickOrTreat(3, candies)).isEqualTo("Trick or treat!");
+        int nbrChildren = 3;
+        String expected = "Trick or treat!";
+
+        String actual = Halloween.trickOrTreat(nbrChildren, candies);
+
+        assertThat(actual).isEqualTo(expected);
     }
 
     @Test
     public void returnsTrickOrTreatWhenChildrenHaveDifferentAmountOfCandiesWithoutDividingPackets() {
         String[][] candies = {{"candy", "candy"}, {"candy", "candy", "candy", "candy", "candy"}, {"candy", "candy", "apple"}};
-        assertThat(Halloween.trickOrTreat(3, candies)).isEqualTo("Trick or treat!");
+        int nbrChildren = 3;
+        String expected = "Trick or treat!";
+
+        String actual = Halloween.trickOrTreat(nbrChildren, candies);
+
+        assertThat(actual).isEqualTo(expected);
     }
 
 }
