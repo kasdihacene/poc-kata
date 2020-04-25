a4123644cb6b662f4303fb25957c143844fb1050
Hacene <hacene.upmc@gmail.com>
2020-04-25 13:16:50 +0200

    refactoring

--- src/main/java/com/mlaku/dev/kata/halloweencandies/Halloween.java
+++ src/main/java/com/mlaku/dev/kata/halloweencandies/Halloween.java
@@ -1,9 +1,12 @@
 package com.mlaku.dev.kata.halloweencandies;
 
 public class Halloween {
-    public static String trickOrTreat(int nbrChldren, String[][] candies) {
-        if (candies.length == 0) return "Trick or treat!";
-        if (nbrChldren == 0) return "Trick or treat!";
-        return "Thank you, strange uncle!";
+
+    public static final String TRICK_OR_TREAT = "Trick or treat!";
+    public static final String THANK_YOU_STRANGE_UNCLE = "Thank you, strange uncle!";
+
+    public static String trickOrTreat(int nbrChildren, String[][] candies) {
+        if (candies.length == 0 || nbrChildren == 0) return TRICK_OR_TREAT;
+        return THANK_YOU_STRANGE_UNCLE;
     }
 }
