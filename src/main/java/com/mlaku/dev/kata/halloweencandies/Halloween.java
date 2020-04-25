8539c323c8779dee74a35983e87b75843da30fa6
Hacene <hacene.upmc@gmail.com>
2020-04-25 15:18:12 +0200

    refactoring step 2

--- src/main/java/com/mlaku/dev/kata/halloweencandies/Halloween.java
+++ src/main/java/com/mlaku/dev/kata/halloweencandies/Halloween.java
@@ -9,27 +9,27 @@ public class Halloween {
 
     public static final String TRICK_OR_TREAT = "Trick or treat!";
     public static final String THANK_YOU_STRANGE_UNCLE = "Thank you, strange uncle!";
+    public static final String BOMB = "bomb";
+    public static final String CANDY = "candy";
 
     public static String trickOrTreat(int nbrChildren, String[][] candies) {
         if (nbrChildren < 2 || candies.length != nbrChildren) return TRICK_OR_TREAT;
 
         Optional<String[]> bombPacket = Arrays.stream(candies)
-                .filter(packet -> Arrays.stream(packet).filter(element -> element.equals("bomb")).findFirst().isPresent())
+                .filter(packet -> Arrays.stream(packet).anyMatch(element -> element.equals(BOMB)))
                 .findFirst();
         if (bombPacket.isPresent()) return TRICK_OR_TREAT;
 
-        List<Long> nbrC = Arrays.stream(candies)
-                .map(packet -> Arrays.stream(packet).filter(element -> element.equals("candy")).count())
+        List<Long> nbrCandies = Arrays.stream(candies)
+                .map(packet -> Arrays.stream(packet).filter(element -> element.equals(CANDY)).count())
                 .collect(Collectors.toList());
-        Long candiesFirstPacket = nbrC.get(0);
-        Optional<Long> childHasDifferentAmount = nbrC.stream().filter(nbrCandiesOPacket -> hasDifferentAmountCandies(nbrCandiesOPacket < 2, nbrCandiesOPacket != candiesFirstPacket)).findFirst();
+
+        Optional<Long> childHasDifferentAmount = nbrCandies.stream()
+                .filter(nbrCandiesOPacket -> nbrCandiesOPacket < 2 || nbrCandiesOPacket != nbrCandies.get(0))
+                .findFirst();
         if (childHasDifferentAmount.isPresent()) return TRICK_OR_TREAT;
         return THANK_YOU_STRANGE_UNCLE;
 
-    }
-
-    private static boolean hasDifferentAmountCandies(boolean b, boolean b2) {
-        return b || b2;
     }
 
 }
--- src/test/java/com/mlaku/dev/kata/halloweencandies/HalloweenCandiesTest.java
+++ src/test/java/com/mlaku/dev/kata/halloweencandies/HalloweenCandiesTest.java
@@ -11,21 +11,6 @@ public class HalloweenCandiesTest {
         assertThat(true).isTrue();
     }
 
-    // management rules :
-
-    /**
-     * - must have 2 or more children in the args
-     * - if children have what they want return "Thank you, strange uncle!"
-     * - else return "Trick or treat!"
-     * <p>
-     * 1 - Each child has at least two candies;
-     * 2 - Each child gets the same amount of candy.
-     * 3 - No children get the "bomb" ;-)
-     * 4 - Packets cannot be divided, each child gets a full packet
-     * <p>
-     * Parameteriazed tests
-     */
-
     @Test
     public void returnsThankYouWhenHaving2packetsWith2candiesFor2children() {
         String result = Halloween.trickOrTreat(2, new String[][]{{"candy", "apple", "candy"}, {"candy", "candy"}});
@@ -82,7 +67,7 @@ public class HalloweenCandiesTest {
 
     @Test
     public void returnsTrickOrTreatWhenChildrenHaveDifferentAmountOfCandiesWithoutDividingPackets() {
-        String[][] candies = {{"candy", "candy"}, {"candy", "candy","candy", "candy","candy"}, {"candy", "candy", "apple"}};
+        String[][] candies = {{"candy", "candy"}, {"candy", "candy", "candy", "candy", "candy"}, {"candy", "candy", "apple"}};
         assertThat(Halloween.trickOrTreat(3, candies)).isEqualTo("Trick or treat!");
     }
 
