be9badcbb85ce4cffe622d67f59c6e14aa3e1a5e
Hacene <hacene.upmc@gmail.com>
2020-05-06 12:54:55 +0200

    adding context when fetching items in list articles

--- src/main/java/com/mlaku/dev/kata/basketshop/Result.java
+++ src/main/java/com/mlaku/dev/kata/basketshop/Result.java
@@ -1,10 +1,11 @@
 package com.mlaku.dev.kata.basketshop;
 
-public class Result {
+public abstract class Result {
 
     public static Result articleFound(Price price){
         return new FoundResult(price);
     }
+
     public static Result articleNotFound(String itemCode){
         return new NotFoundResult(itemCode);
     }
